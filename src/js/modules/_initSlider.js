fd290c95bcee6aca7ffb4f86b6909e4f90152db7
Andrey Eferemov <samo.dur.andre@gmail.com>
2023-02-06 13:07:37 +0300

    feat: добавил разметку для всех блоков кроме каталога

--- src/js/modules/_initSlider.js
+++ src/js/modules/_initSlider.js
@@ -2,8 +2,8 @@
 import Swiper, {Navigation, Pagination, Autoplay, Lazy, FreeMode, Keyboard, Thumbs, Mousewheel} from 'swiper';
 export function initSlider() {
 	// Swiper.use([Navigation, Pagination, Autoplay, Lazy, FreeMode]);
-	const port = new Swiper('.portfolio__slider-container', {
-		modules:[Navigation, Pagination, Autoplay, Lazy, FreeMode, Keyboard],
+	const port = new Swiper('.feedback__slider-container', {
+		modules:[Navigation, Pagination, Autoplay, FreeMode, Keyboard],
 		loop: true,
 		speed: 1000,
 		spaceBetween: 30,
@@ -16,10 +16,6 @@ export function initSlider() {
 			nextEl: '.portfolio__next',
 			prevEl: '.portfolio__prev',
 		},
-		lazy: {
-			loadPrevNext: true,
-			loadPrevNextAmount: 2,
-		},
 		autoplay: {
 			delay: 6000,
 		},
@@ -28,45 +24,9 @@ export function initSlider() {
 		},
 		breakpoints: {
 			569: {
-				slidesPerView: 3,
-				slidesPerGroup: 3,
+				slidesPerView: 1,
 				spaceBetween: 20,
 			},
 		},
 	});
-
-	const swiper = new Swiper('.modal-slider-container-mini', {
-		modules:[Navigation, Pagination, Autoplay, Lazy, FreeMode, Keyboard, Mousewheel],
-		loop: true,
-		mousewheel: true,
-		spaceBetween: 10,
-		speed: 1000,
-		freeMode: true,
-		watchSlidesProgress: true,
-		lazy: true,
-		breakpoints: {
-			320: {
-				slidesPerView: 3,
-			},
-			569: {
-				slidesPerView: 6,
-			},
-		},
-	});
-
-	const swiper2 = new Swiper('.modal-slider-container', {
-		modules:[Navigation, Pagination, Autoplay, Lazy, FreeMode, Keyboard, Thumbs, Mousewheel],
-		loop: true,
-		mousewheel: true,
-		spaceBetween: 10,
-		speed: 1000,
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-		thumbs: {
-			swiper: swiper,
-		},
-		lazy: true
-	})
 }
