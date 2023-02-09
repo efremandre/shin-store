fc1219aa6131a4cc5003d4570d6c7c1f0941ba02
Andrey Eferemov <samo.dur.andre@gmail.com>
2023-02-09 10:25:37 +0300

    feat: добавил очистку в форму php

--- dist/vendor/sendtg/send.php
+++ dist/vendor/sendtg/send.php
@@ -1,7 +1,10 @@
 <?php
 $name = $_POST['name'];
+$name = str_replace(['(',')','{','}','[', ']', '+', '=', '$', '_'], '', $name );
 $phone = $_POST['tel'];
+$phone = str_replace(['(',')','-','+',' '], '', $phone );
 $message = $_POST['message'];
+$message = str_replace(['(',')','{','}','[', ']', '+', '=', '$', '_'], '', $message );
 $token = "6106008047:AAEdB2ZQy2RmNGf7mqQMnG6mqu-V2BMLkw0";
 $chat_id = "-1001769675176";
 $arr = array(
@@ -11,7 +14,7 @@ $arr = array(
 );
 
 foreach($arr as $key => $value) {
-    $txt .= "<b>".$key."</b> ".$value."%0A";
+    $txt .= "<b>".$key."</b>".$value."%0A";
 };
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
@@ -19,7 +22,7 @@ $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_i
 if (!$sendToTelegram) {
     $message = 'Что-то пошло не так и ничего не отправилось...';
 } else {
-    $message = 'Ваша заявка принята!';
+    $message = 'Ваша заявка отправлена!';
 }
 
 $response = ['message' => $message];
