fb1e4768fbb01382200c4b387b04b81ea7a6a912
Andrey Eferemov <samo.dur.andre@gmail.com>
2023-02-15 09:37:49 +0300

    feat: альфа версия

--- src/vendor/send.php
+++ src/vendor/send.php
@@ -1,10 +1,10 @@
 <?php
 $name = $_POST['name'];
-$name = str_replace(['(',')','{','}','[', ']', '+', '=', '$', '_'], '', $name );
+$name = str_replace(['(',')','{','}','[', ']', '+', '=', '$', '_', '<', '>', '/', '/\/'], '', $name );
 $phone = $_POST['tel'];
 $phone = str_replace(['(',')','-','+',' '], '', $phone );
 $message = $_POST['message'];
-$message = str_replace(['(',')','{','}','[', ']', '+', '=', '$', '_'], '', $message );
+$message = str_replace(['(',')','{','}','[', ']', '+', '=', '$', '_', '<', '>', '/', '/\/'], '', $message );
 $token = "6106008047:AAEdB2ZQy2RmNGf7mqQMnG6mqu-V2BMLkw0";
 $chat_id = "-1001769675176";
 $arr = array(
